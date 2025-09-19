e25fa289ba7978074381ff9cc3e5c6cd65fae20f
askar full belajar <108921033+askar-ef@users.noreply.github.com>
2025-09-19 16:35:41 +0700

    fix: ui visibility

--- FRONTEND/src/app/layout.tsx
+++ FRONTEND/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
 import Providers from './providers';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Quinty DAO - Decentralized Bounty System',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>
           {children}
         </Providers>
